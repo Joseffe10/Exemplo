24ac8c0652fc3c5593121158e9660a3c4eeb510e
Sandyara Peres <40678077+SandyaraPeres@users.noreply.github.com>
2021-03-08 09:12:08 -0300

    Intents

--- app/src/main/java/com/joseffe/exemplo/MainActivity.kt
+++ app/src/main/java/com/joseffe/exemplo/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.joseffe.exemplo
 
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.txtNumero
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var texto : TextView
     private lateinit var botao : Button
+    private lateinit var proximo : Button
+    private lateinit var foto : Button
+    private lateinit var usuario: ImageView
 
     //    val boasVindas: String by lazy {
 //        val texto = "Seja bem vindo!"
@@ -24,12 +31,43 @@ class MainActivity : AppCompatActivity() {
 
         texto = findViewById(R.id.txtNumero)
         botao = findViewById(R.id.btnSortear)
+        proximo = findViewById(R.id.btnProximo)
+        foto = findViewById(R.id.btnFoto)
+        usuario = findViewById(R.id.fotoUsuario)
 
         botao.setOnClickListener {
             SortearNumero()
         }
 
+        proximo.setOnClickListener {
+            val intent = Intent(this@MainActivity, SecondActivity::class.java)
+            val usuario = Usuario(nome = "Maria", id = 125)
+            intent.putExtra("USUARIO", usuario)
+            intent.putExtra("IDADE", 32)
+            startActivity(intent)
+        }
+
+        foto.setOnClickListener {
+            abrirCamera()
+        }
+
         //texto = txtNumero
+    }
+
+    fun abrirCamera(){
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        if(intent.resolveActivity(packageManager) != null){
+            startActivityForResult(intent, 12345)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 12345 && resultCode == RESULT_OK){
+            val fotoTirada = data?.extras?.get("data") as Bitmap
+            fotoUsuario.setImageBitmap(fotoTirada)
+        }
     }
 
     override fun onBackPressed() {
--- app/src/main/java/com/joseffe/exemplo/SecondActivity.kt
+++ app/src/main/java/com/joseffe/exemplo/SecondActivity.kt
@@ -7,5 +7,12 @@ class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        //INTENT EXPLÍCITA
+        val idade = intent.extras?.get("IDADE")
+
+        val usuario = intent.extras?.get("USUARIO") as Usuario
+        val nomeUsuario = usuario.nome
+
     }
 }
